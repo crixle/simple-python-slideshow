b6872608dcd80b147a4453e762c46fa571235284
Crixle <54859942+crixle@users.noreply.github.com>
2022-10-25 09:57:16 -0400

    Update python.py
    Add comments and user variables

--- python.py
+++ python.py
@@ -5,6 +5,7 @@ import webbrowser
 import logging
 app = Flask(__name__)
 
+READ_DIR = 'images/' ## <--- Change to directory of desired images
 
 def gen():
     i = 0
@@ -14,28 +15,28 @@ def gen():
         time.sleep(refreshInterval)
         images = get_all_images()
         image_name = images[i]
-        im = open('images/' + image_name, 'rb').read()
+        im = open(READ_DIR + image_name, 'rb').read()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + im + b'\r\n')
         i += 1
         if i >= len(images):
             i = 0
 
-        logging.warning('Streamed image')
+        ## logging.warning('Streamed image') <--- Optional | Only if you want debug confirmation it's finding images
         updateRefresh += 1
         if updateRefresh == 2:
             logging.warning("Hit 2 threshold")
-            refreshInterval = 180
+            refreshInterval = 180   ## <-- Change to desired 
 
 
 
+ ## Reads directory and turns the paths into a list, then returns them to be incrementally displayed
 def get_all_images():
-    image_folder = "images"
-    images = [img for img in os.listdir(image_folder)
+    images = [img for img in os.listdir(READ_DIR)
               if img.endswith(".jpg") or
               img.endswith(".jpeg") or
-              img.endswith("png") or
-              img.endswith("gif")]
+              img.endswith(".png") or
+              img.endswith(".gif")]
     return images
 
 
@@ -52,4 +53,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True)
+    app.run(host='127.0.0.1', debug=False
